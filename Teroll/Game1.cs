307f61868f74740f6a097eceb5cb5bf3355e50a0
Tobias PC Win11 <tobis-stroedicke@gmx.de>
2026-06-20 17:07:44 +0200

    Player Movement + FPS

--- Teroll/Game1.cs
+++ Teroll/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Teroll
 {
@@ -9,6 +10,12 @@ namespace Teroll
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        public Player Player { get; private set; }
+        private Texture2D _playerTexture;
+
+#if DEBUG
+        private SpriteFont _debugFont;
+#endif
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -19,6 +26,11 @@ namespace Teroll
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            IsFixedTimeStep = true;
+            //TargetElapsedTime = TimeSpan.FromSeconds(1d / 60);
+            
+            _graphics.SynchronizeWithVerticalRetrace = true;
+            _graphics.ApplyChanges();
 
             base.Initialize();
         }
@@ -28,23 +40,57 @@ namespace Teroll
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            _playerTexture = Content.Load<Texture2D>("Player\\Player");
+            Player = new Player(_playerTexture, new Vector2(100, 100));
+
+#if DEBUG
+            _debugFont = Content.Load<SpriteFont>("Debug\\DebugFont");
+#endif
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
+#if DEBUG
+            if (Keyboard.GetState().IsKeyDown(Keys.R))
+            {
+                maxFPS = double.MinValue;
+                minFPS = double.MaxValue;
+            }
+#endif
             // TODO: Add your update logic here
+            Player.Update(gameTime);
 
             base.Update(gameTime);
         }
-
+#if DEBUG
+        // MAX MIN FPS
+        double maxFPS = double.MinValue;
+        double minFPS = double.MaxValue;
+#endif
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            Player.Draw(_spriteBatch);
+#if DEBUG
+            double currentFPS = 1 / gameTime.ElapsedGameTime.TotalSeconds;
+            if(currentFPS > maxFPS)
+                maxFPS = currentFPS;
+            if(currentFPS < minFPS)
+                minFPS = currentFPS;
+            _spriteBatch.DrawString(
+                _debugFont,
+                "Current " + ((int)(1 / gameTime.ElapsedGameTime.TotalSeconds)).ToString() + "\nMin " + ((int)minFPS).ToString() + "\nMax " + ((int)maxFPS).ToString(),
+                new Vector2(10, 10),
+                Color.White);
+#endif
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ Teroll/Player.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Teroll
+{
+    public class Player
+    {
+        private Texture2D _texture;
+        private Vector2 _position;
+        private Vector2 _velocity;
+
+        private float _moveSpeed = 200f;       // Sofort Max-Speed
+        private float _slowSpeed = 100f;       // Shift gedrückt
+        private float _gravity = 900f;
+        private float _maxYVelovity = 500;
+        private float _jumpStrength = -350f;
+
+        private bool _isOnGround = false;
+        private int _jumpsLeft = 2;            // Double-Jump
+
+        public Player(Texture2D texture, Vector2 startPos)
+        {
+            _texture = texture;
+            _position = startPos;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            var k = Keyboard.GetState();
+
+            // --- Movement ---
+            float speed = k.IsKeyDown(Keys.LeftShift) ? _slowSpeed : _moveSpeed;
+
+            if (k.IsKeyDown(Keys.Left))
+                _velocity.X = -speed;
+            else if (k.IsKeyDown(Keys.Right))
+                _velocity.X = speed;
+            else
+                _velocity.X = 0; // Sofort stehen bleiben
+
+            // --- Jump ---
+            if (k.IsKeyDown(Keys.Space) && _isOnGround)
+            {
+                _velocity.Y = _jumpStrength;
+                _isOnGround = false;
+                _jumpsLeft = 1; // Double-Jump übrig
+            }
+            else if (k.IsKeyDown(Keys.Space) && _jumpsLeft > 0 && _velocity.Y > 0)
+            {
+                _velocity.Y = _jumpStrength;
+                _jumpsLeft--;
+            }
+
+            // --- Gravity ---
+            _velocity.Y += _gravity * dt;
+            _velocity.Y = Math.Min(_velocity.Y, _maxYVelovity);
+
+            // --- Apply movement ---
+            _position += _velocity * dt;
+
+            // --- Simple ground collision (Test) ---
+            if (_position.Y >= 400)
+            {
+                _position.Y = 400;
+                _velocity.Y = 0;
+                _isOnGround = true;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _position, Color.White);
+
+        }
+    }
+}
